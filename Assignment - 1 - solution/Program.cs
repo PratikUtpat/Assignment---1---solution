4947c592646441ea800c6497b92bdcfa5ce84bf7
pratik <pratik.utpat@gmail.com>
2019-01-27 20:23:18 -0500

    Add project files.

---
+++ Assignment - 1 - solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment___1___solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
